57eba65498e52afe7e0c6e3b3ccca3b14eaba96f
nmghelani <nevilghelani99@gmail.com>
2021-12-03 18:16:03 -0500

    Fixed accessibility in home & learn

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,25 +1,25 @@
-import React from 'react'
-import {Nav, NavLink, NavMenu} from './NavbarElements'
-import logo from '../../images/logo.svg'
+import React from "react";
+import { Nav, NavLink, NavMenu } from "./NavbarElements";
+import logo from "../../images/logo.svg";
 const Navbar = () => {
-    return (
-        <>
-        <Nav>
-            <NavLink to="/">
-                <img src={logo} alt='logo' width="180" height="180" />
-            </NavLink> 
-            
-            <NavMenu>   
-                <NavLink to="/quiz" activeStyle>
-                    <h3>Quiz</h3>   
-                </NavLink>
-                <NavLink to="/about" activeStyle>
-                    <h3>Reviews</h3>   
-                </NavLink> 
-            </NavMenu> 
-        </Nav>
-        </>
-    )
-}
+  return (
+    <>
+      <Nav>
+        <NavLink to="/">
+          <img src={logo} alt="logo" width="180" height="180" />
+        </NavLink>
+
+        <NavMenu>
+          <NavLink to="/quiz" activeStyle>
+            <span className="bold">Quiz</span>
+          </NavLink>
+          <NavLink to="/about" activeStyle>
+            <span className="bold">Reviews</span>
+          </NavLink>
+        </NavMenu>
+      </Nav>
+    </>
+  );
+};
 
 export default Navbar;
